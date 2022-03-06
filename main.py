92da27fd35135542115c367c46c94c8881e70949
lucienlo <m0952520083@gmail.com>
2022-03-07 04:09:06 +0800

    [proto] 20220306v2 daily update

---
+++ main.py
@@ -0,0 +1,52 @@
+import json
+import os, threading
+
+import shioaji as sj
+import pandas as pd
+from shioaji import BidAskSTKv1, Exchange
+
+def login(api, acc, pwd):
+  api.login(
+    person_id = acc, 
+    passwd = pwd, 
+    contracts_cb = lambda security_type: print(f"{repr(security_type)} fetch done.")
+  )
+
+def logout(api):
+  api.logout()
+
+def subscribe_bidAsk(api, sid):
+  api.quote.subscribe(
+    api.Contracts.Stocks[sid], 
+    quote_type = sj.constant.QuoteType.Tick,
+    version = sj.constant.QuoteVersion.v1,
+    intraday_odd = True
+  )
+  api.quote.subscribe(
+    api.Contracts.Stocks[sid],
+    quote_type = sj.constant.QuoteType.Tick,
+    version = sj.constant.QuoteVersion.v1,
+    intraday_odd = False
+  )
+
+def quote_callback(exchange: Exchange, bidask:BidAskSTKv1):
+    print(f"Exchange: {exchange}, BidAsk: {bidask}")
+
+def main():
+  api = sj.Shioaji()
+  with open('./secret.json') as file:
+    info = json.load(file)
+
+  print('login with: ' + info['acc'])
+  login(api, info['acc'], info['pwd'])
+
+  odd_ba = subscribe_bidAsk(api, "2330")
+  api.quote.set_on_bidask_stk_v1_callback(quote_callback)
+
+  print("all setting done, it's listening...")
+  threading.Event().wait()
+
+  logout(api)
+
+if __name__ == "__main__":
+  main()
